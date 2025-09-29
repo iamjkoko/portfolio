af8d98dce2e60673b07259c2a925d1c6feae4253
Eric Ko <203650735+iamjkoko@users.noreply.github.com>
2025-09-30 06:59:38 +0900

    arch

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../global.css';
-import styles from '../styles/Archive.module.css';
+import styles from '../styles/archive.module.css';
 
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../constants/routes';
