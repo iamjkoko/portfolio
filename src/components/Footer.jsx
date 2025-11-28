ab6904158c18e7545504a59cd9a967fcbe431444
Eric Ko <203650735+iamjkoko@users.noreply.github.com>
2025-11-28 21:53:35 +0900

    footer-v3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = ({ theme }) => {
             <span className="opacity-60">
             <CornerDownRight size={16} />
             </span>
-            <span className="transition-opacity opacity-50 hover:opacity-90">
+            <span className="transition-opacity opacity-60 hover:opacity-100">
             eko03@risd.edu
             </span>
           </a>
