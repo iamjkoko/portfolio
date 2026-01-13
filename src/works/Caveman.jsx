bc604ad6b6ecc33008fae6db35ac90174b4fcb98
Eric Ko <203650735+iamjkoko@users.noreply.github.com>
2026-01-13 20:38:52 +0900

    font-update

--- src/works/Caveman.jsx
+++ src/works/Caveman.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../global.css';
-import '../assets/fonts/boska/boska.css';
 import styles from '../styles/works.module.css';
 
 import Footer from '../components/Footer';
